f3c67d53930b150485fd396530a5a7cf340c407b
Christopher Johns <cjohns4288@gmail.com>
2017-11-17 21:11:26 -0800

    Added mouse input and comments

--- core/src/miniventure/game/Game.java
+++ core/src/miniventure/game/Game.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class Game extends ApplicationAdapter {
 	
@@ -30,7 +31,13 @@ public class Game extends ApplicationAdapter {
 		tiles = new TextureAtlas("data/tiles.txt");
 		tree = tiles.createSprite("tree");
 		
-		treeBounds = new Rectangle(SCREEN_WIDTH/2 - tree.getWidth()/2, 0, tree.getWidth(), tree.getHeight());
+		treeBounds = new Rectangle(SCREEN_WIDTH/2, 0, tree.getWidth(), tree.getHeight());
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		tiles.dispose();
 	}
 
 	@Override
@@ -42,14 +49,32 @@ public class Game extends ApplicationAdapter {
 		batch.setProjectionMatrix(camera.combined); // tells the batch to use the camera's coordinate system.
 		batch.begin();
 		
-		batch.draw(tree, treeBounds.x, treeBounds.y);
+		batch.draw(tree, treeBounds.x-treeBounds.width/2, treeBounds.y-treeBounds.height/2);
 		
 		batch.end();
+		
+		handleInput();
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		tiles.dispose();
+	private void handleInput() {
+		if(Gdx.input.isTouched()) { // if a mouse button is pressed...
+			/*
+				We want to transform the touch/mouse coordinates to our camera's coordinate system.
+				This is necessary because the coordinate system used for mouse input might be different than the one we use for objects in our world.
+			 */
+			
+			// get the coordinates as a 3D vector, because OrthographicCamera is actually a 3D camera which takes into account z-coordinates as well.
+			Vector3 mousePos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+			
+			// transform these coordinates to our camera's coordinate system (modifies the object)
+			camera.unproject(mousePos);
+			
+			treeBounds.x = mousePos.x;
+			treeBounds.y = mousePos.y;
+			
+			/*
+				Note: it is very, very bad to instantiate a new object all the time, such as the Vector3 instance. The reason for this is the garbage collector has to kick in frequently to collect these short-lived objects. On the desktop it's not such a big deal, but other platforms can experience issues. To solve this issue in this particular case, we can simply make mousePos a field of the Game class instead of instantiating it all the time.
+			 */
+		}
 	}
 }
