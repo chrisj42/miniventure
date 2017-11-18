de4894ca63f030f953f951312f7d4e76828d86d7
Christopher Johns <cjohns4288@gmail.com>
2017-11-17 20:38:51 -0800

    added some homemade sprites, and made the first few customizations, with rendering

--- core/src/miniventure/game/Game.java
+++ core/src/miniventure/game/Game.java
@@ -3,31 +3,53 @@ package miniventure.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	public static final int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 450;
+	
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	
+	private TextureAtlas tiles;
+	private Sprite tree;
+	
+	private Rectangle treeBounds;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
+		
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		tiles = new TextureAtlas("data/tiles.txt");
+		tree = tiles.createSprite("tree");
+		
+		treeBounds = new Rectangle(SCREEN_WIDTH/2 - tree.getWidth()/2, 0, tree.getWidth(), tree.getHeight());
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		camera.update();
+		Gdx.gl.glClearColor(0.1f, 0.5f, 0.1f, 1); // these are floats from 0 to 1.
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.setProjectionMatrix(camera.combined); // tells the batch to use the camera's coordinate system.
 		batch.begin();
-		batch.draw(img, 0, 0);
+		
+		batch.draw(tree, treeBounds.x, treeBounds.y);
+		
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		tiles.dispose();
 	}
 }
--- desktop/src/miniventure/game/desktop/DesktopLauncher.java
+++ desktop/src/miniventure/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import miniventure.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Miniventure";
+		config.width = Game.SCREEN_WIDTH;
+		config.height = Game.SCREEN_HEIGHT;
 		new LwjglApplication(new Game(), config);
 	}
 }
