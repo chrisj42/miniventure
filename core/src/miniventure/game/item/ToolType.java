77ff24465e126113521cca7ec236107c42bc6daf
Christopher Johns <cjohns4288@gmail.com>
2018-01-08 08:19:06 -0800

    so many attempts at making item entities...

--- core/src/miniventure/game/item/ToolType.java
+++ core/src/miniventure/game/item/ToolType.java
@@ -1,10 +1,12 @@
 package miniventure.game.item;
 
+import miniventure.game.GameCore;
+
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public enum ToolType {
 	
 	PICKAXE, SHOVEL, AXE, SWORD;
 	
-	public final TextureRegion texture = null;
+	public final TextureRegion texture = GameCore.icons.get("gem-sword");
 }
