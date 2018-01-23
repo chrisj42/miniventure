6f92f3973a4cf249ede84e2133f338efec71a53e
Christopher Johns <cjohns4288@gmail.com>
2018-01-22 21:30:08 -0800

    removed old code

--- core/src/miniventure/game/screen/RespawnScreen.java
+++ core/src/miniventure/game/screen/RespawnScreen.java
@@ -22,20 +22,4 @@ public class RespawnScreen extends MenuScreen {
 			}
 		}));
 	}
-	
-	/*@Override
-	public void render(float delta) {
-		Gdx.gl.glClearColor(0.1f, 0.5f, 0.1f, 1); // these are floats from 0 to 1.
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		game.getBatch().begin();
-		
-		MyUtils.drawTextCentered(GameCore.getFont(), game.getBatch(),"You Died! Click or press space to respawn.", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		
-		game.getBatch().end();
-		
-		if (Gdx.input.justTouched() || Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
-			game.setScreen(gameScreen);
-	}*/
-	
 }
