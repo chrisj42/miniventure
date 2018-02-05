bc457772ae5578400dad3d723b13b8eb48ea07c7
Christopher Johns <cjohns4288@gmail.com>
2018-02-05 15:40:32 -0800

    Added display bar for tool durability

--- core/src/miniventure/game/item/ToolItem.java
+++ core/src/miniventure/game/item/ToolItem.java
@@ -1,9 +1,16 @@
 package miniventure.game.item;
 
+import miniventure.game.util.MyUtils;
 import miniventure.game.world.WorldObject;
 import miniventure.game.world.entity.mob.Mob;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
 public class ToolItem extends Item {
+	
+	private static final float DURABILITY_BAR_HEIGHT = 4; // 8 pixels.
 	
 	public enum Material {
 		Wood(20, 1, 4),
@@ -69,7 +76,18 @@ public class ToolItem extends Item {
 		return damage * material.damageMultiplier;
 	}
 	
-	// TODO when i have a fishing rod, I can make it use the interact method. Or, I can give the water tile an interactable property. I think that might be cleaner.
+	@Override
+	public void drawItem(int stackSize, Batch batch, BitmapFont font, float x, float y, Color textColor) {
+		super.drawItem(stackSize, batch, font, x, y, textColor);
+		
+		if(durability == material.maxDurability) return; // no bar
+		
+		// draw a colored bar for the durability left
+		float durPerc = durability*1f / material.maxDurability;
+		float width = Item.ICON_SIZE * durPerc;
+		Color barColor = durPerc >= 0.5f ? Color.GREEN : durPerc >= 0.2f ? Color.YELLOW : Color.RED;
+		MyUtils.fillRect(x, y, width, DURABILITY_BAR_HEIGHT, barColor, batch);
+	}
 	
 	@Override
 	public boolean equals(Object other) {
