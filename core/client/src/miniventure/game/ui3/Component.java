b9f7c00659499143ac02138a1f93e8c9c169cd39
Christopher Johns <cjohns4288@gmail.com>
2018-08-17 20:23:40 -0700

    edited sizing and esentially finished initial component class

--- core/client/src/miniventure/game/ui3/Component.java
+++ core/client/src/miniventure/game/ui3/Component.java
@@ -1,22 +1,54 @@
 package miniventure.game.ui3;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
+
+import org.jetbrains.annotations.NotNull;
 
 public abstract class Component {
 	
-	private Vector2 actualSize, prefSize;
+	private Vector2 actualSize;
+	private Vector2 prefSize;
+	@NotNull private Vector2 position = new Vector2();
 	
+	protected Component() {
+		
+	}
 	
+	public abstract void render(Batch batch);
 	
-	protected void invalidate() {
+	public void invalidate() {
 		prefSize = null;
 	}
 	
-	final void setSize(Vector2 size) {
-		actualSize.set(size);
-		configureForSize(size);
+	
+	public void setPosition(@NotNull Vector2 position) {
+		this.position.set(position);
+	}
+	
+	public Vector2 getPosition() { return position.cpy(); }
+	
+	
+	public final void setSize(Vector2 size) {
+		if(actualSize == null) {
+			if(size != null)
+				actualSize = size.cpy();
+		}
+		else {
+			if(size == null)
+				actualSize = null;
+			else
+				actualSize.set(size);
+		}
+		configureForSize(getSize());
 	}
 	protected abstract void configureForSize(Vector2 size);
+	
+	public Vector2 getSize() {
+		if(actualSize == null)
+			actualSize = getPrefSize();
+		return actualSize.cpy();
+	}
 	
 	public Vector2 getPrefSize() {
 		if(prefSize == null)
