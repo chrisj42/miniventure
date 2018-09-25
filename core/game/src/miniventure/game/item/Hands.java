f522647dc5923fa8f487e2a4e2d70af7b6f4aa16
Christopher Johns <cjohns4288@gmail.com>
2018-09-25 17:38:55 -0400

    edits to convert to new use of hotbar and unlimited inv

--- core/game/src/miniventure/game/item/Hands.java
+++ core/game/src/miniventure/game/item/Hands.java
@@ -1,54 +1,86 @@
 package miniventure.game.item;
 
+import java.util.Arrays;
+
+import miniventure.game.util.MyUtils;
+
 import org.jetbrains.annotations.NotNull;
 
-public class Hands extends Inventory {
+public class Hands {
 	
 	// holds the items in the player's hotbar.
 	
-	private int selection;
-	private Inventory inventory;
+	private static final int MAX_ITEM_COUNT = 5;
 	
-	Hands(@NotNull Inventory inventory) {
-		super(3);
-		this.inventory = inventory;
+	private static final HandItem hand = new HandItem(); // ref var so it isn't constantly re-instantiated.
+	
+	private int selection;
+	private Inventory playerInventory;
+	
+	// a hashmap really isn't worth it.
+	// private final HashMap<Item, Integer> itemPositions = new HashMap<>();
+	private Item[] slots;
+	
+	Hands(@NotNull Inventory playerInventory) {
+		this.playerInventory = playerInventory;
+		slots = new Item[MAX_ITEM_COUNT];
+		reset();
 	}
 	
-	@Override
+	public void reset() {
+		Arrays.fill(slots, hand);
+	}
+	
+	public int getSize() { return slots.length; }
+	
 	public boolean addItem(Item item) {
-		if(super.getCount(item) == 0) {
-			if(!super.addItem(item))
-				return inventory.addItem(item);
-		} else {
-			if(!inventory.addItem(item))
-				return super.addItem(item);
+		if(item == null) return false;
+		
+		for(int i = 0; i < slots.length; i++) {
+			if(slots[i].equals(item))
+				return false;
+			if(slots[i].equals(hand)) {
+				slots[i] = item;
+				return true;
+			}
 		}
 		
-		return true; // first of the two choices succeeded
+		return false; // no empty slots
 	}
 	
-	@Override
 	public boolean removeItem(Item item) {
-		if(inventory.removeItem(item))
-			return true;
-		return super.removeItem(item);
+		for(int i = 0; i < slots.length; i++) {
+			if(slots[i].equals(item)) {
+				slots[i] = hand;
+				return true;
+			}
+		}
+		return false;
 	}
 	
-	@Override
-	public int getCount(Item item) { return super.getCount(item) + inventory.getCount(item); }
+	
+	public String[] save() {
+		String[] data = new String[getSize()];
+		for(int i = 0; i < data.length; i++)
+			data[i] = MyUtils.encodeStringArray(slots[i].save());
+		
+		return data;
+	}
+	
+	public void loadItems(String[] allData) {
+		reset();
+		for(String data: allData)
+			addItem(Item.load(MyUtils.parseLayeredString(data)));
+	}
+	
 	
 	public void setSelection(int idx) { selection = idx; }
 	public int getSelection() { return selection; }
-	
-	void swapItem(Inventory inv, int invIdx, int hotbarIdx) {
-		Item item = inv.replaceItemAt(invIdx, getItemAt(hotbarIdx));
-		replaceItemAt(hotbarIdx, item);
-	}
 	
 	public void resetItemUsage() {}
 	
 	public boolean hasUsableItem() { return !(getSelectedItem().isUsed()); }
 	
 	@NotNull
-	public Item getSelectedItem() { return getItemAt(selection); }
+	public Item getSelectedItem() { return slots[selection]; }
 }
--- core/game/src/miniventure/game/item/Inventory.java
+++ core/game/src/miniventure/game/item/Inventory.java
@@ -1,113 +1,70 @@
 package miniventure.game.item;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 import miniventure.game.util.InstanceCounter;
 import miniventure.game.util.MyUtils;
 
-public class Inventory {
+public abstract class Inventory {
 	
 	private static final HandItem hand = new HandItem(); // ref var so it isn't constantly re-instantiated.
 	
-	private Item[] items;
-	private final InstanceCounter<Item> itemCounter = new InstanceCounter<>();
-	
-	public Inventory(int size) {
-		items = new Item[size];
-		reset();
-	}
+	private final LinkedHashMap<Item, Integer> items = new LinkedHashMap<>();
 	
 	public void reset() {
-		Arrays.fill(items, hand);
-		itemCounter.clear();
-		itemCounter.put(hand, items.length);
+		items.clear();
 	}
 	
-	public int getSlots() { return items.length; }
-	public int getFilledSlots() { return getSlots() - itemCounter.get(hand); }
+	public int getSize() { return items.size(); }
+	public int getCount(Item item) { return items.getOrDefault(item, 0); }
 	
-	public int getCount(Item item) { return itemCounter.get(item); }
+	public Item[] getItems() { return items.keySet().toArray(new Item[0]); }
+	
 	public boolean hasItem(Item item) { return hasItem(item, 1); }
 	public boolean hasItem(Item item, int count) { return getCount(item) >= count; }
 	
-	public Item[] getItems() {
-		Item[] items = new Item[this.items.length];
-		System.arraycopy(this.items, 0, items, 0, items.length);
-		return items;
-	}
-	
-	public boolean addItem(Item item) {
-		int openIdx = getFirstMatch(hand);
-		if(openIdx < 0) return false; // no empty slots
+	public boolean addItem(Item item) { return addItem(item, 1); }
+	public boolean addItem(ItemStack itemStack) { return addItem(itemStack.item, itemStack.count); }
+	public boolean addItem(Item item, int count) {
+		if(!itemCounter.containsKey(item))
+			items.add(item);
 		
-		replaceItemAt(openIdx, item);
-		return true;
 	}
-	public boolean removeItem(Item item) {
-		int idx = getFirstMatch(item);
-		if(idx < 0) return false; // item not found
+	
+	public boolean removeItem(Item item, boolean all) {
+		if(!itemCounter.containsKey(item))
+			return false;
 		
-		replaceItemAt(idx, hand);
-		return true;
+		if(itemCounter.get(item) == 1 || all) {
+			items.remove(item);
+			itemCounter.remove(item);
+			return true;
+		}
+		else {
+			itemCounter.removeInstance(item);
+			return true;
+		}
 	}
 	
-	private int getFirstMatch(Item item) { return getFirstMatch(0, item); }
-	private int getFirstMatch(int startIdx, Item item) {
-		for(int i = startIdx; i < items.length; i++)
-			if(items[i].equals(item))
-				return i;
+	Item getItemAt(int idx) { return items.get(idx); }
+	
+	Item replaceItemAt(int idx, ItemStack item) {
 		
-		return -1;
 	}
-	
-	private int getLastMatch(Item item) { return getLastMatch(0, item); }
-	private int getLastMatch(int startIdx, Item item) {
-		for(int i = Math.min(startIdx, items.length-1); i >= 0; i--)
-			if(items[i].equals(item))
-				return i;
-		
-		return -1;
-	}
-	
-	Item getItemAt(int idx) {
-		checkIndex(idx);
-		return items[idx];
-	}
-	
-	Item replaceItemAt(int idx, Item item) {
-		checkIndex(idx);
-		// okay to replace with hand item.
-		if(item == null)
-			item = hand;
-		
-		Item cur = items[idx];
-		itemCounter.removeInstance(cur);
-		itemCounter.add(item);
-		items[idx] = item;
-		return cur;
-	}
-	
-	private void checkIndex(int idx) {
-		if(idx >= items.length) throw new IndexOutOfBoundsException("cannot access inventory index " + idx + "; inventory only contains " + items.length + " items.");
-	}
-	
 	
 	public String[] save() {
-		String[] data = new String[items.length];
-		for(int i = 0; i < data.length; i++) {
-			data[i] = MyUtils.encodeStringArray(items[i].save());
-		}
+		String[] data = new String[getSize()];
+		for(int i = 0; i < data.length; i++)
+			data[i] = MyUtils.encodeStringArray(ItemStack.save(items.get(i), ));
 		
 		return data;
 	}
 	
 	public void loadItems(String[] allData) {
-		itemCounter.clear();
-		for(int i = 0; i < items.length; i++) {
-			String[] data = MyUtils.parseLayeredString(allData[i]);
-			items[i] = Item.load(data);
-			itemCounter.add(items[i]);
-		}
+		reset();
+		for(String data: allData)
+			loadItem(MyUtils.parseLayeredString(data));
 	}
 	
 }
