9578ad73888c52ed45fac1f7b8c9f78e7b7b8433
Christopher Johns <cjohns4288@gmail.com>
2018-10-05 01:05:52 -0400

    added mouse listener to link label to change the cursor to a hand on hover (though it didn't seem to work for me)

--- core/client/src/miniventure/game/screen/MyLinkLabel.java
+++ core/client/src/miniventure/game/screen/MyLinkLabel.java
@@ -1,10 +1,30 @@
 package miniventure.game.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Cursor.SystemCursor;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.kotcrab.vis.ui.widget.LinkLabel;
 
 public class MyLinkLabel extends LinkLabel {
+	
+	{
+		addListener(new InputListener() {
+			@Override
+			public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+				Gdx.graphics.setSystemCursor(SystemCursor.Hand);
+			}
+			
+			@Override
+			public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+				Gdx.graphics.setSystemCursor(SystemCursor.Arrow);
+			}
+		});
+	}
+	
 	public MyLinkLabel(CharSequence url) {
 		super(url);
 	}
