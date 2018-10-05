48621397a378c1a007513bf69b6c21d3eccffe9d
Christopher Johns <cjohns4288@gmail.com>
2018-10-05 00:36:35 -0400

    removed old code

--- core/client/src/miniventure/game/screen/MenuScreen.java
+++ core/client/src/miniventure/game/screen/MenuScreen.java
@@ -70,10 +70,6 @@ public class MenuScreen extends Stage {
 		Container<Actor> box = addComponent(comp, space);
 		box.center();
 	}
-	/*protected void addSpaced(Actor comp, int space) {
-		vGroup.addActor(comp);
-		// if(space > 0) add(Box.createVerticalStrut(space));
-	}*/
 	
 	protected static VisLabel makeLabel(String text) {
 		VisLabel label = new VisLabel(text, new LabelStyle(GameCore.getFont(), Color.WHITE));
@@ -81,38 +77,6 @@ public class MenuScreen extends Stage {
 		label.setAlignment(Align.center, Align.left);
 		return label;
 	}
-	
-	/*protected static VisTextButton makeButton(String text, Action onClick) {
-		VisTextButton button = new VisTextButton(text);
-		button.addActionListener(e -> onClick.act());
-		// button.setFont(ClientCore.DEFAULT_FONT);
-		return button;
-	}*/
-	
-	@Override
-	public String toString() {
-		return getClass().getSimpleName()+"@"+Integer.toHexString(hashCode());
-	}
-	
-	/*protected static class LabelPanel extends JPanel {
-		
-		public LabelPanel() {
-			setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-			setOpaque(false);
-			setBackground(new Color(163, 227, 232, 200));
-			setBorder(BorderFactory.createCompoundBorder(
-			  BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.CYAN, Color.GRAY),
-			  BorderFactory.createEmptyBorder(20, 30, 20, 20)
-			));
-		}
-		
-		@Override
-		protected void paintComponent(final Graphics g) {
-			g.setColor(getBackground());
-			g.fillRect(0, 0, getWidth(), getHeight());
-			super.paintComponent(g);
-		}
-	}*/
 	
 	protected <T extends Actor> Container<T> addComponent(T comp) { return addComponent(0, comp, 0); }
 	protected <T extends Actor> Container<T> addComponent(int spacing, T comp) { return addComponent(spacing, comp, 0); }
@@ -125,5 +89,10 @@ public class MenuScreen extends Stage {
 		box.padBottom(postSpacing);
 		vGroup.addActor(box);
 		return box;
+	}
+	
+	@Override
+	public String toString() {
+		return getClass().getSimpleName()+"@"+Integer.toHexString(hashCode());
 	}
 }
