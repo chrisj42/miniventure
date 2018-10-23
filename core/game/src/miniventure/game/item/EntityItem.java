2582eac5849e7ff9bbc4a79fd0716654ab7dbb75
Christopher Johns <cjohns4288@gmail.com>
2018-10-22 20:01:08 -0400

    added furniture classes?

---
+++ core/game/src/miniventure/game/item/EntityItem.java
@@ -0,0 +1,24 @@
+package miniventure.game.item;
+
+import miniventure.game.world.entity.Entity;
+
+/**
+ * An item that, when placed in the world, becomes an entity.
+ * This is similar to TileItems, which become tiles when placed.
+ */
+public class EntityItem extends Item {
+	
+	public EntityItem(String entityData) {
+		
+	}
+	
+	@Override
+	public String[] save() {
+		return new String[0];
+	}
+	
+	@Override
+	public Item copy() {
+		return null;
+	}
+}
