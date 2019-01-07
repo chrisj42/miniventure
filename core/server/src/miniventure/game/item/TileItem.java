4c9b6faf892e8202b5737a0010f1af5c768458d3
chrisj42 <cjohns4288@gmail.com>
2019-01-06 23:30:52 -0800

    abstracted tileitem placement check into an interface; placement checks can now be move advanced than a simple list of allowed tile types. Also allowed stone path to be placed on all stone (may remove later).

--- core/server/src/miniventure/game/item/TileItem.java
+++ core/server/src/miniventure/game/item/TileItem.java
@@ -1,6 +1,5 @@
 package miniventure.game.item;
 
-import java.util.Arrays;
 import java.util.EnumMap;
 
 import miniventure.game.GameCore;
@@ -19,12 +18,37 @@ public class TileItem extends ServerItem {
 	
 	private static final EnumMap<TileTypeEnum, TileItem> items = new EnumMap<>(TileTypeEnum.class);
 	
+	@FunctionalInterface
+	private interface PlacementCheck {
+		
+		boolean canPlace(ServerTile tile, Player player);
+		
+		static PlacementCheck on(@Nullable TileTypeEnum... canPlaceOn) {
+			return (tile, player) -> {
+				boolean canPlace = canPlaceOn == null;
+				if(!canPlace) {
+					TileTypeEnum tileType = tile.getType().getTypeEnum();
+					for (TileTypeEnum underType: canPlaceOn) {
+						if(underType == tileType) {
+							canPlace = true;
+							break;
+						}
+					}
+				}
+				return canPlace;
+			};
+		}
+	}
+	
 	private static final TileTypeEnum[] groundTypes = new TileTypeEnum[] {
 		TileTypeEnum.GRASS, TileTypeEnum.DIRT, TileTypeEnum.SAND, TileTypeEnum.SNOW, TileTypeEnum.STONE_FLOOR, TileTypeEnum.STONE_PATH
 	};
 	
 	private static void addItem(TileTypeEnum result, TileTypeEnum... canPlaceOn) {
-		items.put(result, new TileItem(result, canPlaceOn));
+		items.put(result, new TileItem(result, PlacementCheck.on(canPlaceOn)));
+	}
+	private static void addItem(TileTypeEnum result, PlacementCheck placementCheck) {
+		items.put(result, new TileItem(result, placementCheck));
 	}
 	
 	static {
@@ -37,53 +61,48 @@ public class TileItem extends ServerItem {
 		addItem(TileTypeEnum.GRASS, TileTypeEnum.DIRT);
 		addItem(TileTypeEnum.STONE, TileTypeEnum.DIRT);
 		
-		items.put(TileTypeEnum.CLOSED_DOOR, new TileItem("Door", GameCore.tileAtlas.findRegion("closed_door/c00"), TileTypeEnum.CLOSED_DOOR, groundTypes));
+		items.put(TileTypeEnum.CLOSED_DOOR, new TileItem("Door", GameCore.tileAtlas.findRegion("closed_door/c00"), TileTypeEnum.CLOSED_DOOR, PlacementCheck.on(groundTypes)));
 		items.put(TileTypeEnum.OPEN_DOOR, items.get(TileTypeEnum.CLOSED_DOOR));
 		
-		items.put(TileTypeEnum.DIRT, new TileItem("Dirt", GameCore.icons.get("items/dirt"), TileTypeEnum.DIRT, TileTypeEnum.HOLE));
+		items.put(TileTypeEnum.DIRT, new TileItem("Dirt", GameCore.icons.get("items/dirt"), TileTypeEnum.DIRT, PlacementCheck.on(TileTypeEnum.HOLE)));
 		
 		addItem(TileTypeEnum.STONE_FLOOR, TileTypeEnum.HOLE/*, TileTypeEnum.DIRT*/);
 		
-		addItem(TileTypeEnum.STONE_PATH, TileTypeEnum.DIRT, TileTypeEnum.SAND, TileTypeEnum.GRASS, TileTypeEnum.SNOW);
+		PlacementCheck pathTiles = PlacementCheck.on(TileTypeEnum.DIRT, TileTypeEnum.SAND, TileTypeEnum.GRASS, TileTypeEnum.SNOW, TileTypeEnum.STONE, TileTypeEnum.STONE2, TileTypeEnum.STONE3, TileTypeEnum.STONE4);
+		addItem(TileTypeEnum.STONE_PATH, (tile, player) -> {
+			if(!pathTiles.canPlace(tile, player)) return false;
+			// todo check that the player tile is at a similar height to the target tile.
+			// fixme issue: player can break adjacent stone after placing path, which provides a way around the above check.
+			return true;
+		});
 	}
 	
 	@NotNull
 	public static TileItem get(@NotNull TileTypeEnum tile) {
 		if(!items.containsKey(tile))
-			items.put(tile, new TileItem(tile, groundTypes));
+			items.put(tile, new TileItem(tile, PlacementCheck.on(groundTypes)));
 		return items.get(tile).copy();
 	}
 	
 	@NotNull private final TileTypeEnum result;
-	@Nullable private final TileTypeEnum[] canPlaceOn;
+	@NotNull private final PlacementCheck placementCheck;
 	
-	private TileItem(@NotNull TileTypeEnum type, @Nullable TileTypeEnum... canPlaceOn) {
-		this(MyUtils.toTitleCase(type.name()), GameCore.tileAtlas.findRegion(type.name().toLowerCase()+"/c00"), type, canPlaceOn); // so, if the placeOn is null, then...
+	private TileItem(@NotNull TileTypeEnum type, @NotNull PlacementCheck placementCheck) {
+		this(MyUtils.toTitleCase(type.name()), GameCore.tileAtlas.findRegion(type.name().toLowerCase()+"/c00"), type, placementCheck);
 	}
 	
-	private TileItem(String name, @NotNull TextureHolder texture, @NotNull TileTypeEnum result, @Nullable TileTypeEnum... placeOn) {
+	private TileItem(String name, @NotNull TextureHolder texture, @NotNull TileTypeEnum result, @NotNull PlacementCheck placementCheck) {
 		super(ItemType.Tile, name, texture);
-		this.canPlaceOn = placeOn;
 		this.result = result;
+		this.placementCheck = placementCheck;
 	}
 	
 	@Override
 	public Result interact(WorldObject obj, Player player) {
 		if(obj instanceof ServerTile) {
 			ServerTile tile = (ServerTile) obj;
-			boolean canPlace = canPlaceOn == null;
-			if(!canPlace) {
-				for (TileTypeEnum underType : canPlaceOn) {
-					if(underType == tile.getType().getTypeEnum()) {
-						canPlace = true;
-						break;
-					}
-				}
-			}
-			
-			if(canPlace && tile.addTile(ServerTileType.get(result))) {
+			if(placementCheck.canPlace(tile, player) && tile.addTile(ServerTileType.get(result)))
 				return Result.USED;
-			}
 		}
 		
 		return obj.interactWith(player, this);
@@ -92,18 +111,18 @@ public class TileItem extends ServerItem {
 	@Override
 	public boolean equals(Object other) {
 		if(!super.equals(other)) return false;
-		TileItem ti = (TileItem) other;
-		return Arrays.equals(canPlaceOn, ti.canPlaceOn) && ti.result == result;
+		TileItem ti = (TileItem) other; // honestly should never get to this point; items that create the same tile will be part of a different system, not instances of this class.
+		return ti.result == result;
 	}
 	
 	@Override
 	public int hashCode() {
-		return super.hashCode() - 17 * Arrays.hashCode(canPlaceOn) + result.hashCode();
+		return super.hashCode() - 17 * result.hashCode();
 	}
 	
 	@Override
 	public TileItem copy() {
-		return new TileItem(getName(), getTexture(), result, canPlaceOn);
+		return new TileItem(getName(), getTexture(), result, placementCheck);
 	}
 	
 	@Override
